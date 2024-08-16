38d6f6157825a780f2d96b44a89b4a868f9048f9
Karthik Sarode <Karthik.Sarode@dayforce.com>
2024-08-16 18:09:21 +0530

    added a json object

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 require('dotenv').config()
+
 const express = require('express')
 
 const app = express()
+
+const someJSON = {
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}
 
 app.get('/', (req, res) => {
     res.send('Karthik Sarode')
@@ -13,6 +37,10 @@ app.get('/twitter', (req, res) => {
 
 app.get('/login', (req, res) => {
     res.send('<h1>Login here for the fun')
+})
+
+app.get('/sampleJSON', (req, res) => {
+    res.json(someJSON)
 })
 
 app.listen(process.env.PORT, () => {
